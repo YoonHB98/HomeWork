5aefc45447f91553302186c408c94b8ada3f099d
윤형빈 <wudqls98@gmail.com>
2022-01-12 17:49:55 +0900

    윤형빈 1월 12일 홀수 짝수

---
+++ Homework/HomeWork220112/HomeWork220112.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+	
+
+	{
+		int Arr[10] = { 0, };
+		int Index = 0;
+		while (Index < 10) //짝수
+		{
+			
+			Arr[Index] = Index * 2 + 2; //Index에 2를 곱하고 거기에2를 더함(0번이 2가 됨)
+			Index += 1; // Index 1 증가
+			
+		}
+	}
+
+	{
+		int Arr[10] = { 0, };
+		int Index = 0;
+		while (Index < 10) //홀수
+		{
+			
+			Arr[Index] = Index * 2 + 1; // Index에 2를 곱하고 거기에 1을 더함(0번이 1이 됨)
+			Index += 1; // Index 1 증가
+			
+		}
+
+		int a = 0;
+	}
+
+}
