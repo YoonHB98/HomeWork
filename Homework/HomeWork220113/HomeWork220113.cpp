2df41aa9d8b669f339fb6533f59b03ec77521b51
윤형빈 <wudqls98@gmail.com>
2022-01-13 17:07:03 +0900

    윤형빈 220113 숙제

---
+++ Homework/HomeWork220113/HomeWork220113.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+void StringTrimRemove(const char* _Left, char* _Right)
+{
+    
+    int i = 0;
+    int Left = 0;
+    int Right = 0;
+    while (i < 10000)
+    {    
+        
+        if (_Left[Left] != ' ')
+        {
+            _Right[Right] = _Left[Left];
+          
+           Left += 1;
+           Right += 1;
+        }
+        else
+        {
+            Left += 1;
+        }
+        i += 1;
+    }
+    
+    return;
+}
+
+int main()
+{
+    char Text[10000] = { 0,};
+    StringTrimRemove("a d    dd dd cc  g yu", Text);
+    // "aaabbbcccddd"
+    printf_s(Text);
+}
+
