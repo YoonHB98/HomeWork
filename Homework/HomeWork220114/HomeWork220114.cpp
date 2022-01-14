4da4cdcdb2f40b740ae757bb2643abc6abe36437
윤형빈 <wudqls98@gmail.com>
2022-01-14 17:24:16 +0900

    윤형빈 220113 숙제

---
+++ Homework/HomeWork220114/HomeWork220114.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+void ToUpper(const char* _Left, char* _Right)
+{
+
+	int i = 0;
+	while (_Left[i] != 0)
+	{
+		if (_Left[i] >= 97)//소문자면
+		{
+			_Right[i] = _Left[i] - 32;
+			i += 1;
+
+		}
+		else//그 외에
+		{
+			_Right[i] = _Left[i];
+			i += 1;
+
+		}
+		
+
+	}
+	return;
+}
+
+int main()
+{
+	{
+		char Text[100] = {0, };
+
+		ToUpper("12aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPQqRrsStTuUvVwWxXyYzZ", Text);
+		
+		printf_s(Text);
+	}
+}
